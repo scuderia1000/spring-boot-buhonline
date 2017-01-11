13eecbbaae3ada55675dd21b1b1a565622c90f67
Ershov-V-V <a4cbepk@gmail.com>
2017-01-11 16:46:11 +0500

    From rab

--- src/main/java/com/valentin/ershov/domain/Price.java
+++ src/main/java/com/valentin/ershov/domain/Price.java
@@ -12,12 +12,37 @@ public class Price {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String name;
-
     private String unit;
-
     private boolean isCalculated;
+    private Integer base_price_id;
+    private int multipler;
+    //    private String base_price_name;
+
+
+//    public String getBase_price_name() {
+//        return base_price_name;
+//    }
+//
+//    public void setBase_price_name(String base_price_name) {
+//        this.base_price_name = base_price_name;
+//    }
+//
+    public Integer getBase_price_id() {
+        return base_price_id;
+    }
+
+    public void setBase_price_id(Integer base_price_id) {
+        this.base_price_id = base_price_id;
+    }
+
+    public int getMultipler() {
+        return multipler;
+    }
+
+    public void setMultipler(int multipler) {
+        this.multipler = multipler;
+    }
 
     public boolean getIsCalculated() {
         return isCalculated;
