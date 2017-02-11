2fd15e3bf83987728cc5169e910246c131796069
scuderia1000 <a4cbepk@gmail.com>
2017-02-11 08:08:46 +0500

    Home

---
+++ src/main/java/com/valentin/ershov/repository/Repository.java
@@ -0,0 +1,9 @@
+package com.valentin.ershov.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Valek on 11.02.2017.
+ */
+public interface Repository extends CrudRepository{
+}
