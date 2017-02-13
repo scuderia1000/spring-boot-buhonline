0950842dcc31cb86eff395741a4f24dc57f03d29
Ershov-V-V <a4cbepk@gmail.com>
2017-02-13 16:56:50 +0500

    From rab

--- src/main/java/com/valentin/ershov/repository/Repository.java
+++ src/main/java/com/valentin/ershov/repository/Repository.java
@@ -2,8 +2,10 @@ package com.valentin.ershov.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.io.Serializable;
+
 /**
  * Created by Valek on 11.02.2017.
  */
-public interface Repository extends CrudRepository{
+public interface Repository<T, ID extends Serializable> extends CrudRepository<T, ID>{
 }
